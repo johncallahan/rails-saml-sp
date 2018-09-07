987ab21ea056072ab2268900a2869100b75128cd
John Callahan <jcallahan@acm.org>
2018-09-07 17:11:21 -0400

    comment out issuer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,7 +309,7 @@ Devise.setup do |config|
   config.saml_configure do |settings|
     settings.assertion_consumer_service_url     = ENV["ASSET_HOST"] + "/users/saml/auth"
     settings.name_identifier_format             = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-#    settings.issuer                             = ENV["ENTITY_ID"]
+    settings.issuer                             = ENV["ENTITY_ID"]
     settings.idp_entity_id                      = ENV["SP_NAME"]
     settings.idp_sso_target_url                 = ENV["IDP_SSO_URL"]
     settings.idp_slo_target_url                 = ENV["IDP_SLO_URL"]
